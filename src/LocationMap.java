a37f2164be1ae3affbb2dbe27e81bef7555db741
Firas A <64994269+FirasAleem@users.noreply.github.com>
2021-12-11 02:26:21 +0300

    Fixed LocationMap for handling early test 1

--- src/LocationMap.java
+++ src/LocationMap.java
@@ -4,8 +4,8 @@ import java.util.*;
 //class that behaves like a map
 public class LocationMap implements Map<Integer, Location> {
 
-    private static final String LOCATIONS_FILE_NAME =  "locations.txt";
-    private static final String DIRECTIONS_FILE_NAME =  "directions.txt";
+    private static final String LOCATIONS_FILE_NAME = "locations.txt";
+    private static final String DIRECTIONS_FILE_NAME = "directions.txt";
 
     /*
      * create a static locations HashMap
@@ -32,9 +32,10 @@ public class LocationMap implements Map<Integer, Location> {
          * put each location in the locations HashMap using temporary empty hashmaps for exits ???
          */
         int locationsFileLines = 0;
+        ArrayList<Integer> locationsFileLinesArray = new ArrayList<>();
 
-        try(FileReader fileReader = new FileReader(LOCATIONS_FILE_NAME);
-            BufferedReader bufferedReader = new BufferedReader(fileReader)
+        try (FileReader fileReader = new FileReader(LOCATIONS_FILE_NAME);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)
         ) {
             String temp = "Available locations:" + System.lineSeparator();
             fileLogger.log(temp);
@@ -43,17 +44,17 @@ public class LocationMap implements Map<Integer, Location> {
             while ((line = bufferedReader.readLine()) != null) {
                 StringBuilder sb = new StringBuilder();
                 String[] lineArray = line.split(",");
-                sb.append(lineArray[0]);
+                sb.append(lineArray[0]); //the location as a number e.g. 82 or 95
                 sb.append(": ");
                 sb.append(lineArray[1]);
 
-                for (int i = 2; i < lineArray.length ; i++) {
+                for (int i = 2; i < lineArray.length; i++) {
                     sb.append(",");
                     sb.append(lineArray[i]);
                 }
                 StringBuilder newSB = new StringBuilder();
                 newSB.append(lineArray[1]);
-                for (int i = 2; i < lineArray.length ; i++) {
+                for (int i = 2; i < lineArray.length; i++) {
                     newSB.append(",");
                     newSB.append(lineArray[i]);
                 }
@@ -65,10 +66,18 @@ public class LocationMap implements Map<Integer, Location> {
                 fileLogger.log(sb.toString());
                 consoleLogger.log(sb.toString());
                 locationsFileLines++;
+                boolean addedToArray = false;
+                for (Integer integer : locationsFileLinesArray) {
+                    if (integer.equals(Integer.parseInt(lineArray[0]))) {
+                        addedToArray = true;
+                    }
+                }
+                if (!addedToArray) {
+                    locationsFileLinesArray.add(Integer.parseInt(lineArray[0]));
+                }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
         }
-
         /*
          * Read from DIRECTIONS_FILE_NAME so that a user can move from A to B, i.e. current location to next location
          * use try-with-resources/catch block for the FileReader
@@ -77,8 +86,8 @@ public class LocationMap implements Map<Integer, Location> {
          * check the ExpectedOutput files
          * for each location, create a new location object and add its exit ??????
          */
-        try(FileReader fileReader2 = new FileReader(DIRECTIONS_FILE_NAME);
-            BufferedReader bufferedReader2 = new BufferedReader(fileReader2)
+        try (FileReader fileReader2 = new FileReader(DIRECTIONS_FILE_NAME);
+             BufferedReader bufferedReader2 = new BufferedReader(fileReader2)
         ) {
             String line;
             String temp = "Available directions:" + System.lineSeparator();
@@ -103,32 +112,22 @@ public class LocationMap implements Map<Integer, Location> {
                 fileLogger.log(sb.toString());
                 consoleLogger.log(sb.toString());
 
-                for (int i = 0; i < locationsFileLines; i++) {
-                    if (Integer.parseInt(lineArray[0]) == i){
-                        Location locationToBeEdited = locations.get(i);
+                for (int x : locationsFileLinesArray) {
+                    if (Integer.parseInt(lineArray[0]) == x) {
+                        Location locationToBeEdited = locations.get(x);
                         int locationIDToBeEdited = locationToBeEdited.getLocationId();
                         String descriptionToBeEdited = locationToBeEdited.getDescription();
                         exits = (LinkedHashMap<String, Integer>) locationToBeEdited.getExits();
                         exits.put(lineArray[1], Integer.parseInt(lineArray[2])); //can i use addExits?
                         Location newLocation = new Location(locationIDToBeEdited, descriptionToBeEdited, exits);
-                        locations.put(i, newLocation);
+                        locations.put(x, newLocation);
                         break;
                     }
                 }
-
-                /*HashMap<String, Integer> tempExitsMap = new HashMap<>();
-                tempExitsMap.put(lineArray[1], Integer.parseInt(lineArray[2]));
-                Location tempLoc = new Location((Integer.parseInt(lineArray[0])), (lineArray[1]), tempExitsMap);
-                locations.put(lineCounter, tempLoc);
-                lineCounter++;*/
             }
-
-        }catch (Exception e){
-
+        } catch (Exception e) {
         }
-
     }
-
     /**
      * implement all methods for Map
      * @return
@@ -165,8 +164,6 @@ public class LocationMap implements Map<Integer, Location> {
 
     @Override
     public Location remove(Object key) {
-        //Location deleted = locations.get((Integer) key);
-        //locations.remove(key);
         return locations.remove(key);
     }
 
